fb8ee11140275d447df648b0ca747fc6dc9aba29
carlos santillan <carlos_sks@hotmail.es>
2021-01-26 22:23:07 -0300

    se resolvio el ejercicio 8

---
+++ js/ejercicio8.js
@@ -0,0 +1,7 @@
+//Escribe un programa que pida un número y diga si es divisible por 2 
+let num1 = parseInt(prompt("Elija un numero"));
+if (num1 % 2 == 0) {
+    document.write("El numero es divisible en dos");
+} else {
+    document.write("El numero no es divisible en dos");
+}
