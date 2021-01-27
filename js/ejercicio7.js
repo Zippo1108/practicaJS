93693839e2718147ffb42e2d1c530987d80f866b
carlos santillan <carlos_sks@hotmail.es>
2021-01-26 22:02:53 -0300

    se resolvio el ejercicio 7

--- js/ejercicio7.js
+++ js/ejercicio7.js
@@ -2,3 +2,10 @@
 let num1 = parseInt(prompt("Elija el primer numero"));
 let num2 = parseInt(prompt("Elija el segundo numero"));
 let num3 = parseInt(prompt("Elija el tercer numero"));
+if (num1 > num2 && num1 > num3){
+    document.write("El primer numero es el mayor: " + num1);
+} else if ( num2 > num1 && num2 > num3){
+    document.write("El segundo numero es el mayor: " + num2);
+} else{
+    document.write("el tercer numero es el mayor " + num3);
+}
