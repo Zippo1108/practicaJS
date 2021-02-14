effcc80af08beb1e9d23cd52d13f04e91f48f686
carlos santillan <carlos_sks@hotmail.es>
2021-02-14 13:32:13 -0300

    Se resolvio el ejercicio 11

--- js/ejercicio11.js
+++ js/ejercicio11.js
@@ -1,1 +1,11 @@
-//Añadir al ejercicio anterior que nos diga por cuál de los cuatro es divisible (hay que decir todos por los que es divisible)
+//Añadir al ejercicio anterior que nos diga por cuál de los cuatro es divisible
+let num = parseInt(prompt('Ingrese un numero'));
+if((num % 2) == 0){
+    document.write('Divisible por 2');
+} else if((num % 3) == 0){
+    document.write('Divisble por 3');
+} else if ((num % 5) == 0){
+    document.write('Divisble por 5');
+} else if ((num % 7) == 0){
+    document.write('Divisble por 7');
+}
